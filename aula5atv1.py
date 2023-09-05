7088c127c0c264465dd531e38a01fe9eff834d6c
mgeambastiane <mgeambastiane@gmail.com>
2023-09-05 18:56:30 -0300

    Feature: ADD elif e else

--- aula5atv1.py
+++ aula5atv1.py
@@ -1,7 +1,12 @@
-idade = int(input("Digite sua idade"))
+# idade = int(input("Digite sua idade"))
+idade = 18
+
 
 if idade >= 18:
     print("Indivíduo possui idade mínima para dirigir")
+
+elif idade >= 16 and idade <= 17:
+    print("Indivíduo tem entre 17 e 18 anos e ainda NÃO está apto para dirigir")
 
 else:
     print("Indivíduo NÃO possui idade mínima para dirigir")
