e15ccb42e5da6b72db9d3c3bf252ca76a23efea2
mgeambastiane <mgeambastiane@gmail.com>
2023-09-05 18:39:11 -0300

    Feature: Adicionando o else para a condição de dirigir

--- aula5atv1.py
+++ aula5atv1.py
@@ -2,3 +2,7 @@ idade = int(input("Digite sua idade"))
 
 if idade >= 18:
     print("Indivíduo possui idade mínima para dirigir")
+
+else:
+    print("Indivíduo NÃO possui idade mínima para dirigir")
+
