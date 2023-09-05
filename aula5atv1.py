a4f602c4f50bd9e043a3f505acaf97081507f1cf
mgeambastiane <mgeambastiane@gmail.com>
2023-09-05 18:33:34 -0300

    Aula 5 atividade 1

---
+++ aula5atv1.py
@@ -0,0 +1,4 @@
+idade = int(input("Digite sua idade"))
+
+if idade >= 18:
+    print("Indivíduo possui idade mínima para dirigir")
---
+++ estrutura cond.template.py
@@ -0,0 +1,17 @@
+dinheiro = 8
+
+
+if dinheiro > 10 and dinheiro <= 13:
+    print("Você pode comprar um bombom")
+
+elif dinheiro < 10 and dinheiro > 7:
+    if dinheiro == 8:
+        print("Você pode ocmprar um refri")
+    print("Você vai poder ocmprar uma coxinha")
+
+elif dinheiro > 5 or dinheiro == 4:
+    print("Você pode comprar um kibe")
+
+else: 
+    print("Você não pode comprar nada, vá embora")
+
